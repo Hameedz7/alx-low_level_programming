ffcd73f476d3d17369ec4fc4b10453cc3ac62ce1
Hameedz7 <7ameed619@gmail.com>
2023-09-08 03:28:33 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  * Description: using size to print the size of various types.
  *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
@@ -14,6 +14,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n",i sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }

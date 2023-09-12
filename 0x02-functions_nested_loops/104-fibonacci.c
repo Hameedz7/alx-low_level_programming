2a8375348df2f3882c32907297f531cc7e02f309
Hameedz7 <7ameed619@gmail.com>
2023-09-12 11:11:09 -0700

    A

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -28,7 +28,7 @@ int numLength(int num)
  * main - Entry point
  *
  * Description: prints the first 98 Fibonacci numbers
- * 	starting with 1 and 2 followed by a new line
+ * starting with 1 and 2 followed by a new line
  * Return: Always 0 (Success)
 */
 
